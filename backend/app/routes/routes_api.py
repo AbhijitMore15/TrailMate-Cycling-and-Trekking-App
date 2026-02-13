ab8760253b885563f1bed449ce92643d6123bed2
Abhijit More <abhijitmore15114@gmail.com>
2026-02-13 15:16:54 +0530

    Initial Commit

--- backend/app/routes/routes_api.py
+++ backend/app/routes/routes_api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .database.db import SessionLocal
-from .models.route import Route
+from app.database.db import SessionLocal
+from app.models.route import Route
 
 router = APIRouter()
 
